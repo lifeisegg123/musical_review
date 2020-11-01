1c4df8fde654af47e57f4210c5f3ddc0cb8b0a60
lifeisegg <58962402+lifeisegg123@users.noreply.github.com>
2020-11-01 18:53:31 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const test = async function(){
+  const result = await axios.get('http://localhost:5000/test')
+  const res = result.data.success;
+  console.log(res)
+}
 
 function App() {
   return (
@@ -17,6 +25,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={test}>test</button>
       </header>
     </div>
   );
