a811b5b40668ad440ba229bf052bd4a562b12910
lifeisegg <58962402+lifeisegg123@users.noreply.github.com>
2020-11-19 22:52:02 +0900

    fix bug

--- src/reducer/adminReducer.js
+++ src/reducer/adminReducer.js
@@ -12,9 +12,10 @@ const INITIAL_STATE = {
 const reducer = createReducer(INITIAL_STATE, {
   [types.SET_PAGELIST]: (state, action) => (state.pageList = action.pageList),
   [types.SET_CURINFO]: (state, action) => (state.curInfo = action.curInfo),
-  [types.SET_CURPAGE]: (state, action) => (
-    (state.curPage = action.target), (state.deletionList = [])
-  ),
+  [types.SET_CURPAGE]: (state, action) => {
+    state.curPage = action.target;
+    state.deletionList = [];
+  },
   [types.SET_MAXPAGE]: (state, action) => (state.maxPage = action.maxPage),
   [types.SET_DELETIONLIST]: (state, action) =>
     (state.deletionList = action.deletionList),
