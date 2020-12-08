44df794f0a1d71687e0ce86d4b33f63f8d01f553
ljj <leejj2002@naver.com>
2020-12-08 18:01:29 +0900

    mobile layout done

--- src/components/home/SliderItem.js
+++ src/components/home/SliderItem.js
@@ -22,7 +22,7 @@ const SliderItemWrapper = styled.div`
     border-radius: 5px;
     @media only screen and (max-width: 768px) {
       width: 20vw;
-      height: 20vh;
+      height: 19vh;
     }
   }
 `;
